c42e6c9d9d68e9cc52099cb0914e580f3884f3ca
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2022-05-12 16:23:06 +0900

    Create sigmoid.py

---
+++ review/sigmoid.py
@@ -0,0 +1,16 @@
+#import numpy as np
+
+def sigmoid(x):
+  return 1/(1+np.exp(-x))
+
+import numpy as np
+
+x = np.random.randn(10, 2)
+W1 = np.random.randn(2, 4)
+b1 = np.random.randn(4)
+W2 = np.random.randn(4, 3)
+b2 = np.random.randn(3)
+
+h = np.matmul(x, W1) + b1
+a = sigmoid(h)
+s = np.matmul(a, W2) + b2
