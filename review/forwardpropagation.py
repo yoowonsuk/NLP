f057a102c2c37b0015d394f1ef76630d900e0570
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2022-05-13 13:53:00 +0900

    Update forwardpropagation.py

--- review/forwardpropagation.py
+++ review/forwardpropagation.py
@@ -1,6 +1,15 @@
 import numpy as np
+# repeat node
 D, N = 8, 7
 x = np.random.randn(1, D)
 y = np.repeat(x, N, axis=0)
 dy = np.random.randn(N, D)
 dx = np.sum(dy, axis=0, keepdims=True)
+
+# sum node
+D, N = 8, 7
+x = np.random.randn(N, D)
+y = np.sum(x, axis=0, keepdims=True)
+dy = np.random.randn(1, D)
+dx = np.repeat(dy, N, axis=0)
+
